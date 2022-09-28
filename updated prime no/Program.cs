a83fcfe3f9a21627320afbb34985bfe9ce72aba4
Dipteshpatra <dipteshp@moback.com>
2022-09-28 20:05:52 +0530

    Add project files.

---
+++ updated prime no/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace updated_prime_no
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Prime Numbers : ");
+            for (int i = 2; i <= 100; i++)
+            {
+                for (int j = 2; j <= i; j++)
+                {
+
+                    if (i != j && i % j == 0)
+                        break;
+                    else if (i == j && i % j == 0)
+                        Console.Write("\t" + i);
+                }
+            }
+        }
+    }
+}
+
